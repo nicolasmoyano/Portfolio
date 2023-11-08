e95bc50fd887c0d1e7bfb884aba76fd2f13d7759
Nico <info@nicolas-moyano.se>
2023-11-08 15:58:57 +0100

    Changed line-height

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-        <h1 className='text-5xl font-semibold h-screen'>User Experience Designer passionate about holistic, <br />accessible, and inclusive design, based in Seattle.</h1>
+        <h1 className='text-5xl font-semibold leading-normal'>User Experience Designer passionate about holistic, <br />accessible, and inclusive design, based in Seattle.</h1>
     </>
   )
 }
