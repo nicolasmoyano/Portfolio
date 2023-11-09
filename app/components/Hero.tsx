cb7e5d99ab938c5ccdfb3724aefe92a47b70d3cf
Nico <info@nicolas-moyano.se>
2023-11-09 21:21:09 +0100

    change mobile fontsize

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <>
     <section className='flex h-[calc(100vh-100px)] justify-center items-center'>
-        <h1 className='text-[5rem] font-semibold leading-snug'>Hi! I'm Nico, <br /> A product designer @ <span className='text-blue-700'>
+        <h1 className='lg:text-8xl sm:text-2xl font-semibold'>Hi! I'm Nico, <br /> A product designer @ <span className='text-blue-700'>
         Nordnet</span>. Based in Stockholm.
         </h1>
     </section>
