b24d3f3d80a264a64eecae41fc0154e101af3768
Nico <info@nicolas-moyano.se>
2023-11-10 11:03:06 +0100

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className='sticky top-0'>
         <MainNav />
+        </div>
         {children}
         </body>
     </html>
