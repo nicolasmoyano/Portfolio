719cbec0c5efdab9ac7aa5e2dab85bf13d54ba94
Nico <info@nicolas-moyano.se>
2023-11-10 10:53:49 +0100

    commit

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <>
     <section className='flex h-[calc(100vh-100px)] justify-center items-center'>
-        <h1 className='text-[3rem] px-4 sm:text-6xl font-semibold leading-snug'>Hi! I'm Nico, <br /> A product designer @ <span className='text-blue-700'>
+        <h1 className='text-[3rem] px-4 sm:text-6xl font-semibold leading-tight md:leading-snug'>Hi! I'm Nico, <br /> A product designer @ <span className='text-blue-700'>
         Nordnet</span>. <br/> Based in Stockholm.
         </h1>
     </section>
