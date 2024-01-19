b750e83be9b6c8d985df20f8c14e2afc8668198e
nicolasmoyano <info@nicolas-moyano.se>
2024-01-19 14:42:30 +0100

    Updated Resume page

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <div className='flex justify-center items-center'>
+        <div className='inline-block h-10 w-10 rounded-full ring-2 ring-white p-2 m-4'>NM</div>
+        <div className='flex flex-col'>
+        <div className='text-xl'>Nicolas Moyano</div>
+        <div>Product designer</div>
+        </div>
+    </div>
   )
 }
 
